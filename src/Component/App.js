910cfcb3b6fa87235447f1a3a657a4a244adee56
caner <caner@quizco.de>
2021-09-04 22:43:04 +0200

    login form

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import LoginForm from './LoginForm';
 
 
 
 function App (){
 
-    
+const adminUser={
+    email:"admin@admin.com",
+    password:"admin123"
+}
 
-    
+const [user, setUser]=useState({name:"", email:""});
+const [error,setError]=useState("");
+
+const Login=details=>{
+
+    console.log(details);
+}
+const Logout=()=>{
+    console.log("Logout")
+}
 
         return (
             <div className="container">
+
+                {(user.email !=="") ? ( <div>
+                    <h2>Welcome<span>{user.name}</span></h2>
+                </div>): (<LoginForm/>)}
                 
                 login page
 
