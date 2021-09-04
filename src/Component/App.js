127082927e58f1ef2fd77010c7eaf7a106f1d1ab
caner <caner@quizco.de>
2021-09-04 22:14:13 +0200

    first commit

---
+++ src/Component/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+function App (){
+
+    
+
+    
+
+        return (
+            <div className="container">
+                
+                login page
+
+            </div>);
+    
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './Component/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+//import reportWebVitals from './reportWebVitals';
+
+
+
+
+ReactDOM.render(
+
+  <App />,
+ 
+  document.getElementById('root')
+);
+
+
